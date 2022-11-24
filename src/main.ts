907e24585ec88ce1a84f441aa2514f1dcbb701b9
Егор <ItsAwware@yandex.ru>
2022-11-25 00:02:35 +0600

    build: main.ts set up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
+import { AppModule } from './app.module'
+import * as basic_auth from 'express-basic-auth'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  })
+
+  // nginx meme
+  app.set('trust proxy', 1)
+
+  const config = new DocumentBuilder()
+    .setTitle('Документация по API')
+    .setDescription('Refferency Requests API')
+    .setVersion('1.0')
+    .build()
+
+  app.use(
+    ['/docs', '/docs-json'],
+    basic_auth({
+      challenge: true,
+      users: {
+        dev: 'xaf1337@1337!',
+      },
+    }),
+  )
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  )
+
+  await app.listen(process.env.PORT || 3000)
 }
-bootstrap();
+bootstrap()
