8fe1f9aea14b80bfba3f691b65a3b9c969ae4739
Egor <ItsAwware@yandex.ru>
2023-01-06 23:21:16 +0600

    chore: ...

--- src/controllers/quiz/quiz.service.ts
+++ src/controllers/quiz/quiz.service.ts
@@ -1,4 +1,7 @@
+import { QuizDTO } from './dto/quiz.body.dto'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class QuizService {}
+export class QuizService {
+  private send(body: QuizDTO) {}
+}
